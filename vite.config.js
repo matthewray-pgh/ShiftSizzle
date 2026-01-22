ba69e1ed3a9572c69862d2a567fee735eace1bf2
Matthew Ray <matthewray.pgh@gmail.com>
2026-01-21 19:49:04 -0500

    fix: attempt to fix configuration for hosting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Set base to repo name for GitHub Pages
 export default defineConfig({
+  base: '/ShiftSizzle/',
   plugins: [react()],
 });
